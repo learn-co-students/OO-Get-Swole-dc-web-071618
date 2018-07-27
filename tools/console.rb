698f113d3f516d6ad7e5349d7e662c3a3181169d
aubreeabril <aubree.abril@gmail.com>
2018-07-27 09:19:07 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
+# LIFTERS
+chad = Lifter.new("Chad", 250)
+brad = Lifter.new("Brad", 200)
+sara = Lifter.new("Sara", 170)
+# GYMS
+gym1 = Gym.new("Gym 1")
+gym2 = Gym.new("Gym 2")
+gym3 = Gym.new("Gym 3")
 
+# MEMBERSHIPS
+mem1 = Membership.new(chad, gym3, 100)
+mem2 = Membership.new(brad, gym2, 70)
+mem3 = Membership.new(chad, gym1, 150)
+mem4 = Membership.new(sara, gym1, 150)
 binding.pry
+puts nil
