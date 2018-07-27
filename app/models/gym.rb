56606912c8126a19f80205bb30bc945f249904c4
github username <github email address>
2018-07-27 09:42:21 -0400

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,16 +1,32 @@
 class Gym
- ALL = []
 
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
-    ALL << self
+    @@all << self
 
   end
 
   def self.all
-    ALL
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|m| m.gym == self}
+  end
+
+  def lifters
+    memberships.map {|m| m.lifter}
+  end
+
+  def names
+    lifters.map {|l| l.name}
+  end
+
+  def lift_total
+    lifters.map {|l| l.lift_total}.inject(:+)
   end
 
 end
--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,40 @@
 
 class Lifter
 
-
-
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
-
+    @@all << self
   end
 
+  def memberships
+    Membership.all.select {|m| m.lifter == self}
+  end
 
+  def gyms
+    memberships.map {|m| m.gym}
+  end
+
+  def total_cost
+    sum = 0
+    cost_array = memberships.map do |m|
+      m.cost
+    end
+    cost_array.each do |c|
+      sum += c
+    end
+    sum
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_gym(cost, gym)
+    Membership.new(cost, gym, self)
+  end
 
 end
